c45226021ce4f88db3884b91c31a700a91aa2f74
hairbearr <hbrock214@gmail.com>
2025-06-11 16:02:04 -0400

    Installed A* Pathfinding, and created and started editing Enemy Scripts
    Added enemy spawner, and started to work on the Enemy Controller script.

--- Obelisk_clone_0/Assets/_Obelisk/Scripts/Enemy_Scripts/EnemyController.cs
+++ Obelisk_clone_0/Assets/_Obelisk/Scripts/Enemy_Scripts/EnemyController.cs
@@ -1,11 +1,35 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private float directionInRadian;
+    [SerializeField] GameObject player;
+    private Rigidbody2D rb;
+    private Animator animator;
+    [SerializeField] bool playerIsInAggroRange, playerIsInAttackRange, isAttacking, specialAttacking, isPatrolling, canPatrol, isChasing;
+    [SerializeField] Vector3 patrolStart, patrolEnd;
+    [SerializeField] float direction;
+    public EnemySpawner enemySpawner;
+
+    public bool IsInAggroRange
+    {
+        get { return playerIsInAggroRange; }
+        set { playerIsInAggroRange = value; }
+    }
+
+    public bool IsInAttackRange
+    {
+        get { return playerIsInAttackRange; }
+        set { playerIsInAttackRange = value; }
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -13,4 +37,28 @@ public class EnemyController : MonoBehaviour
     {
         
     }
+
+    private void Animate()
+    {
+        switch (direction)
+        {
+            case 0: // do the east stuff
+                break;
+            case 1: // do the north stuff
+                break;
+            case 2: // do the northeast stuff
+                break;
+            case 3: // do the northwest stuff
+                break;
+            case 4: // do the south stuff
+                break;
+            case 5: // do the southEast stuff
+                break;
+            case 6: // do the southwest stuff
+                break;
+            case 7: // do the west stuff
+                break;
+        }
+    }
+
 }
