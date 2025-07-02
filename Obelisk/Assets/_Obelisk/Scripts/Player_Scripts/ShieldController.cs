88cf2c32b9a0f9d5ffcf29fe66f67bdce0d1470d
Harry Brock <hbrock214@gmail.com>
2025-07-01 22:15:22 -0400

    Updated Shield Controller Script
    - Started Deciding on the Shield Block Logic
    - Added int abilityID
    - added float shieldEnergy
    - added bool isBlocking, but will delete it

--- Obelisk/Assets/_Obelisk/Scripts/Player_Scripts/ShieldController.cs
+++ Obelisk/Assets/_Obelisk/Scripts/Player_Scripts/ShieldController.cs
@@ -4,6 +4,9 @@ public class ShieldController : MonoBehaviour
 {
     private PlayerController player;
     private Animator animator;
+    [SerializeField] int abilityID;
+    [SerializeField] float shieldEnergy;
+    [SerializeField] bool isBlocking;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,21 +18,54 @@ public class ShieldController : MonoBehaviour
     void Update()
     {
         Animate();
+
+        if (player.IsBlocking > 0)
+        {
+            if (shieldEnergy > 0) { ShieldBlock(); }
+            if (shieldEnergy <= 0) { ShieldBreak(); }
+        }
+        if(player.IsBlocking <= 0)
+        {
+            LowerShield();
+        }
+
+        // if shieldEnergy > 0
+        // Shield Block
+        // else if shieldEnergy <= 0
+        // Shield Break
     }
 
     private void ShieldBlock()
     {
+        // turn on the Box Collider
+        // block the incoming damage
+        // while shield blocking decrease shieldEnergy per damage done.
+        // have it be a re-useable health bar, basically. 
+    }
 
+    private void LowerShield()
+    {
+        // lower the shield
     }
 
     private void ShieldBreak()
     {
-
+        // disable the player
+        // regen shield energy
+        // re-enable the player
     }
 
-    private void CastAbility(string ability)
+    private void CastAbility()
     {
-
+        if (abilityID != 0)
+        {
+            print("Cast Shield Ability");
+            // use a switch to decide which abilities to cast, have functions to call what ability it is
+        }
+        else
+        {
+            print("No Shield Ability");
+        }
     }
 
     private void Animate()
