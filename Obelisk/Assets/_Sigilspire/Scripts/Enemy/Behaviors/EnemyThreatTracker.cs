4211759d73de314f5bac97db4c87814b92649b64
hairbearr <hbrock214@gmail.com>
2026-01-15 16:56:32 -0500

    Edited Threat Tracker

--- Obelisk/Assets/_Sigilspire/Scripts/Enemy/Behaviors/EnemyThreatTracker.cs
+++ Obelisk/Assets/_Sigilspire/Scripts/Enemy/Behaviors/EnemyThreatTracker.cs
@@ -45,14 +45,6 @@ public class EnemyThreatTracker : NetworkBehaviour, IThreatReceiver
         return bestId;
     }
 
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
